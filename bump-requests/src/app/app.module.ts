ebf39cae41b8545a2391d8bdf2abc8886ca4fd6a
Jean Louis <lehlouisle@gmail.com>
2018-04-14 16:56:25 +0200

    Load events

--- bump-requests/src/app/app.module.ts
+++ bump-requests/src/app/app.module.ts
@@ -6,20 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { EventsComponent } from './events/events.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    EventsComponent,
-    // AppRoutingModule
+    EventsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- bump-requests/src/app/events/events.component.ts
+++ bump-requests/src/app/events/events.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
+
 export class EventsComponent implements OnInit {
+  events: Object[];
 
-  constructor() { }
-
-  ngOnInit() {
+  getEvents(): void {
+    this.dataService.getEvents().subscribe(events => this.events = events['data']);
   }
 
+  ngOnInit() {
+    this.getEvents();
+  }
+  
+  constructor(private dataService: DataService) { }
 }
